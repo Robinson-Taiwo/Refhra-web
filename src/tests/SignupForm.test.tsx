ef45a84637c286a114a1d3c5d41684ca8121eca0
User <user@Taiwos-MacBook-Pro.local>
2025-08-01 10:00:07 +0100

    feat: implemented rendition tests

--- src/tests/SignupForm.test.tsx
+++ src/tests/SignupForm.test.tsx
@@ -1,9 +1,8 @@
 import { describe, test } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import SignupForm from "@/components/forms/SignupForm";
 
 describe("<SignupForm/>", () =>
   test("renders SignupForm component", () => {
     render(<SignupForm />);
-    screen.debug();
   }));
--- src/tests/sign-up.test.tsx
+++ src/tests/sign-up.test.tsx
@@ -1,9 +1,8 @@
 import { describe, test } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render} from "@testing-library/react";
 import SignUp from "@/app/(external)/auth/sign-in/page";
 
 describe("<SignUp/>", () =>
   test("renders SignUp page", () => {
     render(<SignUp />);
-    screen.debug();
   }));
