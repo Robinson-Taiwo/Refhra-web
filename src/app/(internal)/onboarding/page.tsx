c7407fe8571ff7aa98bef725ef8bb022dcaf4701
Oluwarotimi_ <user@Taiwos-MacBook-Pro.local>
2026-02-20 06:31:54 +0100

    fix: fixed onboarding and routing

---
+++ src/app/(internal)/onboarding/page.tsx
@@ -0,0 +1,12 @@
+import OnboardingWrapper from "@/components/OnboardingWrapper";
+import React from "react";
+
+const OnboardingPage = () => {
+  return (
+    <div>
+      <OnboardingWrapper />
+    </div>
+  );
+};
+
+export default OnboardingPage;
--- src/components/onboarding/OnboardingCard.tsx
+++ src/components/onboarding/OnboardingCard.tsx
@@ -1,24 +1,26 @@
 "use client";
 import { X } from "lucide-react";
 import OnboardingNavigator from "./OnboardingNavigator";
+import { Dispatch, SetStateAction } from "react";
+import { useRouter } from "next/navigation";
 
-interface props{
-  closeModal: boolean,
-  setCloseModal: ()=> void
-  
+interface props {
+  closeModal: boolean;
+  setCloseModal: Dispatch<SetStateAction<boolean>>;
 }
 
+const OnboardingCard = ({ setCloseModal, closeModal }: props) => {
+  const router = useRouter();
 
-
-const OnboardingCard = ({setCloseModal}:props) => {
-  
-
-return (
+  return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
       <div className="bg-white  scrollbar-hide overflow-hidden overflow-y-hidden rounded-2xl shadow-lg w-full max-w-4xl md:h-[70vh] flex flex-col relative">
         {/* Close Button */}
         <button
-          onClick={setCloseModal}
+          onClick={() => {
+            setCloseModal(!closeModal);
+            router.push("/dashboard");
+          }}
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-800"
         >
           <X size={20} />
@@ -31,8 +33,6 @@ return (
       </div>
     </div>
   );
+};
 
-
-}
-
-export default OnboardingCard
+export default OnboardingCard;
